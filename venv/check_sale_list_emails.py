9d56b176b625dcd61a122bbc9463fa3a8421989f
zachiota <58188605+zachiota@users.noreply.github.com>
2020-07-05 19:36:32 -0700

    comments

--- venv/check_sale_list_emails.py
+++ venv/check_sale_list_emails.py
@@ -1,3 +1,5 @@
+# This program is called by all_pay.py to send invoices. It can be called alone (using the __name__==('__main_') syntax)
+# to check the live sale data against the master customer list for validity without sending any invoices.
 from venmo_request import venmo_req , venmo_auth
 from paypal_request import paypal_req
 from send_gmail import create_message
@@ -8,13 +10,11 @@ import pandas as pd
 from get_google_sheet import gsheet2df, open_service
 import re
 
-
-
-
 # for custom mails use: '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
 
 # Define a function for
 # for validating an Email
+# NOT USED YET
 def check(email):
     # Make a regular expression
     # for validating an Email
@@ -26,20 +26,21 @@ def check(email):
     return test
 
 
-def load_and_check():
-    root = tk.Tk()
+def load_and_check(): # main loop to check if all required info for live sale is in master customer list, if true then generates merged data table
+    root = tk.Tk() # this is important for some reason to use Tk()
     root.withdraw()
 
+    # this was commented out when we started using Google spreadsheets, but would read an excel file
     # invoice_file_path = filedialog.askopenfilename(title="Choose invoice file:",filetypes=[("Excel files", ".xlsx .xls")])
 
     # invoice_data = pd.read_excel(invoice_file_path)
 
-    MASTER_ID = '1-Et2zIxQq28ZNXkeiWCO976QKlIPOvoFs9VIwIyddhA'
-    SALES_ID = '1tIFKnnx26bIDO0SGnsMADLBIlx35jFObH_gJP_J8JiY'
+    MASTER_ID = '1-Et2zIxQq28ZNXkeiWCO976QKlIPOvoFs9VIwIyddhA' #ID of master customer list google spreadsheet
+    SALES_ID = '1tIFKnnx26bIDO0SGnsMADLBIlx35jFObH_gJP_J8JiY' #ID of live sale google spreadsheet
 
-    invoice_sheet = open_service(SALES_ID, input('Input Live Sale Sheet ID: '))
-    master_sheet = open_service(MASTER_ID, 'Sheet1')
-    invoice_data = gsheet2df(invoice_sheet).drop_duplicates()
+    invoice_sheet = open_service(SALES_ID, input('Input Live Sale Sheet ID: ')) #identify tab of live sale sheet to reference, can use "Live Sale Test" as sandbox
+    master_sheet = open_service(MASTER_ID, 'Sheet1') #gets master customer list
+    invoice_data = gsheet2df(invoice_sheet).drop_duplicates() #converts spreadsheet to pandas dataframe
     print(invoice_data)
     invoice_data["Instagram User"] = invoice_data["Instagram User"].str.lower()
 
@@ -47,42 +48,42 @@ def load_and_check():
 
     # master_data = pd.read_excel(master_file_path)
 
-    master_data = gsheet2df(master_sheet).drop_duplicates()
-    master_data["Instagram User"] = master_data["Instagram User"].str.lower().str.strip()
+    master_data = gsheet2df(master_sheet).drop_duplicates() # gets rid of duplicates in the customer list
+    master_data["Instagram User"] = master_data["Instagram User"].str.lower().str.strip() # cleans up potential extra spaces
     master_data["Payment"] = master_data["Payment"].str.lower().str.strip()
 
-    data = pd.merge(invoice_data, master_data, how='left', on=None, left_on=None, right_on=None,
+    data = pd.merge(invoice_data, master_data, how='left', on=None, left_on=None, right_on=None, #combines the live sale list with the master customer list
                     left_index=False, right_index=False, sort=True,
                     suffixes=('_x', '_y'), copy=True, indicator=False,
                     validate=None)
 
-    error_table_email1 = data["Instagram User"][data["Email"]=='']
+    error_table_email1 = data["Instagram User"][data["Email"]==''] #checks for empty email
     error_table_email2 = data["Instagram User"][data["Email"].isna()]
-    error_table_venmo = data["Instagram User"][data["Venmo Username"].isna() & data["Payment"] == "venmo"]
-    error_table_pay = data["Instagram User"][data["Payment"].isna()]
+    error_table_venmo = data["Instagram User"][data["Venmo Username"].isna() & data["Payment"] == "venmo"] #checks for empty venmo user
+    error_table_pay = data["Instagram User"][data["Payment"].isna()] #checks for empty payment preference
 
-    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_rows', None) #setting to print entire data frame to console (otherwise it abridges)
 
-    stop = False
-    if len(error_table_email1.isnull())>0:
+    stop = False #continues by default
+    if len(error_table_email1.isnull())>0: #stop if no email
         print("The following users have no valid email:")
         print(error_table_email1.unique())
         stop = True
-    elif len(error_table_email2.isnull())>0:
+    elif len(error_table_email2.isnull())>0: #stop if no email
         print("The following users have no valid email:")
         print(error_table_email2.unique())
         stop = True
     else:
         print("All emails found!")
 
-    if len(error_table_venmo) > 0:
+    if len(error_table_venmo) > 0: #stop of no venmo and needed
         print("The following users have no valid venmo:")
         print(error_table_venmo.unique())
         stop = True
     else:
         print("All Venmo found!")
 
-    if len(error_table_pay) > 0:
+    if len(error_table_pay) > 0: #stop if no payment preference
         print("The following users have no payment method listed:")
         print(error_table_pay.unique())
         stop = True
@@ -94,5 +95,5 @@ def load_and_check():
     else:
         return data
 
-if __name__==('__main__'):
+if __name__==('__main__'): #just runs load_and_check
     load_and_check()
